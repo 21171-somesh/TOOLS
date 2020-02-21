1397d46a9085275d204ca6ae5bde9df39df176b9
AbhiY98 <abhishekyadav88846@gmail.com>
2020-02-21 16:50:04 +0530

    testing basic hashing for passwords

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,29 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const num_iterations = 5;
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
   const body = req.body;
-  console.log(body);
-  res.json({
-    name: 'testing routes'
-  })
-})
+  const password = body.password;
+  if(body.mode == 'create') {
+    bcrypt.hash(password, num_iterations, (err, hash) => {
+      if(err) console.log(err);
+      else {
+        console.log(hash);
+        res.json({
+          hash: hash
+        })
+      }
+    });
+  } else {
+    bcrypt.compare(password, hash, (err, ress) => {
+      if(err) console.log(err);
+      else {
+        console.log(ress);
+        res.json({res: ress})
+      }
+    })
+  }
+ })
 
 module.exports = router;
